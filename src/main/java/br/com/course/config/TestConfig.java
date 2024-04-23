d96e9a69f98d800e6d9b2378fdded4f51a368e71
Jackson Nazário Salvador Guimarães <jacksonnsalvador@gmail.com>
2024-04-23 17:08:19 -0300

    Category

--- src/main/java/br/com/course/config/TestConfig.java
+++ src/main/java/br/com/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.course.entities.Category;
 import br.com.course.entities.Order;
 import br.com.course.entities.User;
 import br.com.course.entities.enums.OrderStatus;
+import br.com.course.repositories.CategoryRepository;
 import br.com.course.repositories.OrderRepository;
 import br.com.course.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,6 +41,12 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAINTING_PAYMENT, u1); 
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 	
 	
